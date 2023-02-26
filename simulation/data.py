dab594b5c9301754e706fa280bc8b6853ce3f957
unknown <haaralahti.elias@gmail.com>
2023-02-26 17:39:46 +0200

    Read map points and simulation length from the h5 file

--- simulation/data.py
+++ simulation/data.py
@@ -2,22 +2,18 @@
 All data from CARLA is stored in the HDF5 format and is processed 
 via the h5py library.
 """
+import io
+import ast
+from typing import List, Tuple
 import h5py
-from numpy import ndarray
+from numpy import ndarray, asarray
+from PIL import Image
 from data_models.agent_state import AgentState
 
 
 class DataLoader():
     """
     Abstraction class for reading data from the hdf5 files.
-    Examples:
-
-    sensors/agent contains images
-    state/agent contains x,y,direction
-    sensor_data_for_vehicle_1 = data['sensors/vehicle_1']
-    sensor_data_for_vehicle_2 = data['sensors/vehicle_2']
-    state_data_for_vehicle_1 = data['state/vehicle_1']
-    state_data_for_vehicle_2 = data['state/vehicle_2']
     """
     def __init__(self, file='data.hdf5'):
         self.h5file = h5py.File(file, 'r')
@@ -28,34 +24,61 @@ class DataLoader():
         that occur in the current scene.
         """
         return list(self.h5file['sensors'].keys())
+    
+    def get_simulation_length(self) -> int:
+        data = self.h5file.get("sensors/", 'r')
+        return len(data[list(data)[0]])
 
-    def read_images(self, agent:str, simulation_step: int) -> ndarray:
+    def read_images(self, agent: str, simulation_step: int) -> ndarray:
         """
         Returns agent camera photo at simulation step
         """
-        data = self.h5file.get(f'sensors/{agent}')
-        return data[simulation_step] 
+        frames = self.h5file.get(f'sensors/{agent}', 'r')
+        frame = frames[simulation_step]
+        img = Image.open(io.BytesIO(frame))
+        img_data = asarray(img)
+        # BGR -> RGB
+        return img_data[:,:,::-1]
+    
+    def get_map(self) -> List[Tuple[int, int]]:
+        """
+        Returns all map markers for visualization purposes.
+        """
+        data = self.h5file.get("metadata", 'r')
+        # .value is old syntax. [()] does same now.
+        data = data[()].decode("UTF-8")
+        # Convert bytes to a dictionary
+        data = ast.literal_eval(data)
+        return data['waypoints']
+
 
     def read_agent_state(self, agent: str, simulation_step: int) -> AgentState:
         """
         Returns agent state at simulation step
         """
-        data = self.h5file.get(f'state/{agent}')[simulation_step]
+        agent_vehicle = agent.replace('camera_', 'vehicle_')
+        data = self.h5file.get(f'state/{agent_vehicle}')[simulation_step]
         return AgentState(x=data[0], y=data[1], direction=data[2])
 
     def __del__(self):
         self.h5file.close()
-        
 
+       
 # Main for testing the class.
 if __name__ == "__main__":
     print("Running dataloader as main")
     dataloader = DataLoader()
 
-    agent = dataloader.read_agent_ids()[0]
-    simulation_step = 0
-    image = dataloader.read_images(agent, simulation_step)
-    print(image.shape)
+    sim_length = dataloader.get_simulation_length()
+    print(f"Simulation length: {sim_length}")
 
+    map_points = dataloader.get_map()
+    print(f"Map points len: {len(map_points)}, point 0: {map_points[0]}")
 
-
+    agents = dataloader.read_agent_ids()
+    print(f"Agent ids: {agents}")
+    
+    agent_test = agents[0]
+    SIMULATION_STEP = 0
+    image = dataloader.read_images(agent_test, SIMULATION_STEP)
+    print(f"Image shape: {image.shape}")
