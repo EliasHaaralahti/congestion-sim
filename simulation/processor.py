24f57b18e9e19b97a04df7bb9ac6edd2a2651ce7
unknown <haaralahti.elias@gmail.com>
2023-04-03 12:11:36 +0300

    Turn processor to a class, rework most methods and add utils and intersection analysis

--- simulation/processor.py
+++ simulation/processor.py
@@ -1,135 +1,283 @@
 import math
 from typing import List, Tuple
 from data_models.output_summary import OutputSummary, DetectionData
-from data_models.agent_state import DetectedAgentState
+from data_models.agent_state import DetectedEntityState
+from data_models.world import IntersectionStatus, World, Intersection
 import numpy as np
 
 
-def check_if_crashing(
-        agent: OutputSummary, target: Tuple[int, int]) -> bool:
-    """ TODO
-    Function for checking if the agent is about to crash with other other 
-    agents or detected. Or just detected?
+class Processor():
     """
-    # Basic logic for now: If agent detected, is close enough and velocity > 0
-    # -> crashing.
+    Class that represent a single external processing unit in the simulated network.
+    Each processor is responsible for processing the data of multiple vehicles and 
+    RSUs. The processor will create the '3D world' and then analyze it, creating 
+    the final object that will contain all data and analysis results for visualizing.
+    """
+    def __init__(self, env, data_pipe: dict, 
+                 result_storage_pipe: list, dataloader):
+        self.env = env
+        self.data_pipe = data_pipe
+        self.result_storage_pipe = result_storage_pipe
+        self.dataloader = dataloader
 
-    agent_position = (agent.agent_x, agent.agent_y)
-    distance = math.dist(agent_position, target) # meters
+        # Congestion thresholds as defined in the report.
+        self.threshold_congestigation_medium = 5
+        self.threshold_congestigation_high = 10
+        # meters, how far detections are assumed to be agents.
+        self.threshold_detection_radius = 2
+        self.threshold_congestigation_speed = 11 # km/h
+        # meters, how far away from intersection to be counted as part of intersection
+        self.threshold_within_intersection_range = 15
+        
 
-    crashing = False
-    if distance < 2:
-        crashing = True
-    return crashing
+    def get_distance(self, agent: OutputSummary, target: Tuple[int, int]) -> float:
+        """
+        Calculate distance between agent and a target point (usually detection).
+        Can be used in analysis to for example, calculate if potential for crash.
+        """
+        agent_position = (agent.agent_x, agent.agent_y)
+        distance = math.dist(agent_position, target) # meters
+        return distance
 
-def process_detection(data: DetectionData) -> Tuple[float]:
-    focal_length = 30
-    height_in_frame = data.ymax - data.ymin
-    # TEMPORARY: Whole picture is 640x640. Get this data from somewhere.
-    image_width = 640
-    box_width_center = data.xmin + ((data.xmax - data.xmin) / 2)
-    fov_angle = 90 # Camera fov should be 90.
-    real_height = 0
-    if data.type == "car":
-        real_height = 1500
-    elif data.type == "person":
-        real_height = 1700
+    def get_closest_intersection(self, agent: OutputSummary) -> object:
+        """
+        Returns the closest intersection distance with keys id and location.
+        Location has keys x and y.
+        """
+        closest_intersection = None
+        closest_distance = math.inf
+
+        for intersection in self.dataloader.get_intersections():
+            x = intersection['location']['x']
+            y = intersection['location']['y']
+            distance = self.get_distance(agent, (x,y))
+            if distance < closest_distance:
+                closest_intersection = intersection
+                closest_distance = distance
+
+        # Car is not close to any intersection currently.
+        if closest_distance > self.threshold_within_intersection_range:
+            return None
+        
+        return closest_intersection
+
+    def process_detection(self, data: DetectionData) -> Tuple[float]:
+        focal_length = 30
+        height_in_frame = data.ymax - data.ymin
+        # TEMPORARY: Whole picture is 640x640. Get this data from somewhere.
+        image_width = 640
+        box_width_center = data.xmin + ((data.xmax - data.xmin) / 2)
+        fov_angle = 90 # Camera fov should be 90.
+        real_height = 0
+        if data.type == "car":
+            real_height = 1500
+        elif data.type == "person":
+            real_height = 1700
+
+        # (mm * mm) / px. Distance is in cm!
+        distance = (real_height * focal_length) / height_in_frame
+        distance = distance / 100 # cm to m. In Carla one coordinate unit = 1m
+        width_offset = (box_width_center/image_width * fov_angle) - (fov_angle / 2)
+        return DetectedEntityState(data.id, data.type, distance, width_offset)
+
+    def process_agent_data(self, data: OutputSummary) -> dict:
+        detections: List[DetectionData] = data.detections
+
+        # Process detections
+        processed_detections = []
+        for detection in detections:
+            result = self.process_detection(detection)
+            processed_detections.append(result)
+        return processed_detections
     
-    # (mm * mm) / px. Distance is in cm!
-    distance = (real_height * focal_length) / height_in_frame
-    distance = distance / 100 # cm to m. In Carla one coordinate unit = 1m
-    width_offset = (box_width_center/image_width * fov_angle) - (fov_angle / 2)
-    return DetectedAgentState(data.id, data.type, distance, width_offset)
+    def is_detection_another_agent(self, current_agent_name, agent_names, target_position):
+        for agent_name in agent_names:
+            # Assume car does not detect itself.
+            if agent_name == current_agent_name:
+                continue
 
-
-def process_agent_data(data: OutputSummary) -> dict:
-    detections: List[DetectionData] = data.detections
-
-    # Process detections
-    processed_detections = []
-    for detection in detections:
-        result = process_detection(detection)
-        processed_detections.append(result)
-    return processed_detections
-
-
-def process(detections: dict, data_pipe: dict, result_storage_pipe: list):
-    """
-    Visualize the 'agents' found while processing the image date
-    Color indicates which node was behind the detection.
-    Nodes have a black circle around them
-
-    :param detections: Processed detections, which indicate new agent positions
-                        relative to the node position and direction
-    :param node_data:  Data from the data_pipe, including AgentState inside a dict
-                        for each agent. 
-    """
-    # Dict of agents with fields x,y,direction and detected,
-    # which is array of arrays [[x,y]]
-    processed_agents = {}
-    for agent in detections:
-        state: OutputSummary = data_pipe[agent]
-        node_direction = state.direction
-        processed_agents[agent] = {
-            'x': state.agent_x,
-            'y': state.agent_y,
-            'direction': node_direction,
-            'velocity': state.velocity,
-            'detected': []
-        }
-
-        for detection in detections[agent]:
-            #detected_agent_offset = detection.offset
-
-            # Calculate new agent position based on distance and width offset.
-            # TODO: width offset not implemented. Distance not used
-            # CARLA seems to handle direction according to unit circle.
-            # Therefore x=cos(angle), y=sin(angle)
-            target_angle = node_direction + detection.width_offset
-            #print(f"Node direction: {node_direction}, target angle: {detection.width_offset}")
-            target_x = state.agent_x + (detection.distance *
-                                        np.cos(np.deg2rad(target_angle)))
-            target_y = state.agent_y + (detection.distance *
-                                        np.sin(np.deg2rad(target_angle)))
+            agent_state: OutputSummary = self.data_pipe[agent_name]
+            distance = self.get_distance(agent_state, target_position)
+            if distance <= self.threshold_detection_radius:
+                # Within threshold range, assume agent and detection are same.
+                return False
+        return True
     
-            crashing = check_if_crashing(state, (target_x, target_y))
-            target = [target_x, target_y, crashing, detection.id,
-                      detection.distance, detection.type]
-            processed_agents[agent]['detected'].append( target )
+    def is_collision_warning(self, agent_state: OutputSummary, 
+                    target: Tuple[float, float]) -> Tuple[float, float]:
+        distance = self.get_distance(agent_state, target) # meters
 
-    return processed_agents
+        # source for equation: https://www.ikorkort.nu/en/vk_korkortsfraga_en_23.php
+        # Braking distance equation used: ((v/10)^2)/2,
+        # where v is in km/h and result in meters.
+        velocity_ms = agent_state.velocity # in m/s, convert to km/h
+        velocity_kmh = velocity_ms * 3.6 # km/h
+        braking_distance = ((velocity_kmh / 10) ** 2) / 2 # meters
 
+        # Add one meter to braking distance to account for unknown target velocity
+        # and give some time to brake. The simulation is not capable of detecting 
+        # target velocity over steps.
+        if distance < braking_distance + 1:
+            return distance, True
 
-def get_intersection_status(results):
-    """
-    TODO: Convert data to information about intersection status.
-    """
-    intersection_status = {
-        'car_detection_count': -1,
-        'human_detection_count': -1,
-        'total_cars': -1,
-        'status': -1
-    }
-    return intersection_status
+        return distance, False
 
+    def process(self, detections: dict) -> World:
+        """
+        Visualize the 'agents' found while processing the image date
+        Color indicates which node was behind the detection.
+        Nodes have a black circle around them
 
+        :param detections: Processed detections, which indicate new agent positions
+                            relative to the node position and direction
+        :param node_data:  Data from the data_pipe, including EntityState inside a dict
+                            for each agent. 
+        Returns a list of processed nodes
+        """
+        # Dict of agents with fields x,y,direction and detected,
+        # which is array of arrays [[x,y]]
+        processed_agents = {'agents': {}}
+        for agent_name in detections:
+            state: OutputSummary = self.data_pipe[agent_name]
+            node_direction = state.direction
+            processed_agents['agents'][agent_name] = {
+                'x': state.agent_x,
+                'y': state.agent_y,
+                'is_rsu': state.is_rsu,
+                'intersection': self.get_closest_intersection(state),
+                'direction': node_direction,
+                'velocity': state.velocity,
+                'detected': []
+            }
 
-def processor(env, data_pipe: dict, result_storage_pipe: list):
-    """
-    Dictionary to hold new detected 'agents' for each actual node.
-    Dictionary key corresponds to node that detected the agent.
-    """
-    detected_agents = {}
-    while True:
-        for agent in data_pipe:
-            results = process_agent_data(data_pipe[agent])
-            detected_agents[agent] = results
+            for detection in detections[agent_name]:
+                #detected_agent_offset = detection.offset
 
-        results = process(detected_agents, data_pipe, result_storage_pipe)
-        # TODO: Processing for intersection information, such as how many
-        # agents there actually are. What is the intersection status? And such
-        intersection_status = get_intersection_status(results)
-        results['intersection_status'] = intersection_status
-        result_storage_pipe['processing_results'].append(results)
-        yield env.timeout(1)
+                # Calculate new agent position based on distance and width offset.
+                # CARLA seems to handle direction according to unit circle.
+                # Therefore x=cos(angle), y=sin(angle)
+                target_angle = node_direction + detection.width_offset
+                target_x = state.agent_x + (detection.distance *
+                                            np.cos(np.deg2rad(target_angle)))
+                target_y = state.agent_y + (detection.distance *
+                                            np.sin(np.deg2rad(target_angle)))
+        
+                distance_to_agent, crashing = self.is_collision_warning(state, (target_x, target_y))
+
+                # Decide if detected target is a new target or an another agent.
+                matches_agent = self.is_detection_another_agent(agent_name, detections, (target_x, target_y))
+                # The processed agents must still be kept, as needed for bounding box drawing 
+                # in visualization! Added to label to target matches_agent to allow filtering.
+                target = [target_x, target_y, crashing, distance_to_agent, detection.id,
+                        detection.distance, detection.type, matches_agent]
+                processed_agents['agents'][agent_name]['detected'].append( target )
+
+        return processed_agents
+
+    def get_intersection_statuses(self, world: World) -> List[IntersectionStatus]:
+        """
+        Analyze intersection and get intersection status data object.
+        """
+        statuses: List[IntersectionStatus] = {}
+
+        # Initialize the dict with all intersections.
+        for intersection in self.dataloader.get_intersections():
+            statuses[intersection['id']] = {
+                'id': intersection['id'],
+                'car_count': 0,
+                'human_count': 0,
+                'rsu_count': 0,
+                'speeds': [], # in m/s
+                'status': "low" 
+            }
+
+        # Loop each agent in the scene (cars, RSUs...)
+        for agent in world['agents']:
+            agent_data = world['agents'][agent]
+            agent_intersection: Intersection = agent_data['intersection']
+
+            # Agent is not currently part of any intersection
+            if agent_intersection is None:
+                continue
+                
+            intersection_id = agent_intersection['id']
+
+            # Add agent if not rsu. Rsus do not add
+            if agent_data['is_rsu']:
+                rsu_count = statuses[intersection_id]['rsu_count']
+                rsu_count += 1
+                statuses[intersection_id]['rsu_count'] = rsu_count
+            else: # vehicle
+                vehicle_count = statuses[intersection_id]['car_count']
+                vehicle_count += 1
+                statuses[intersection_id]['car_count'] = vehicle_count
+                # add speed to speeds for congestion analysis.
+                velocity = agent_data['velocity']
+                statuses[intersection_id]['speeds'].append(velocity)
+
+            # Add detections
+            for detection in agent_data['detected']:
+                detection_type = detection[5]
+                if detection_type == "car":
+                    vehicle_count = statuses[intersection_id]['car_count']
+                    vehicle_count += 1
+                    statuses[intersection_id]['car_count'] = vehicle_count
+                else: # Assume human
+                    human_count = statuses[intersection_id]['human_count']
+                    human_count += 1
+                    statuses[intersection_id]['human_count'] = human_count
+
+        # Revise the status.
+        for _, intersection in statuses.items():
+            car_count = intersection['car_count']
+
+            # If car count is 0, the intersection status does not need to be updated.
+            if car_count == 0:
+                continue
+
+            human_count = intersection['human_count']
+            # Total count tries to reflect the fact that cars are more likely
+            # to cause congestion than humans, which may not cross the roads.
+            total_count = car_count + (human_count / 3)
+            # calculate average speed and convert m/s to km/h
+            average_speed = (sum(intersection['speeds']) / len(intersection['speeds'])) * 3.6
+            
+            # Compare average speed to speed threshold.
+            speed_too_low = (average_speed) <= self.threshold_congestigation_speed
+            # Congestion is low by default. See if high/medium conditions met.
+            if total_count >= self.threshold_congestigation_high and speed_too_low:
+                intersection['status'] = "high"
+            elif total_count >= self.threshold_congestigation_medium and speed_too_low:
+                intersection['status'] = "medium"
+
+        return statuses
+
+    def analyze(self, world: World):
+        world['intersection_statuses'] = self.get_intersection_statuses(world)
+        return world
+
+    def run(self):
+        """
+        Dictionary to hold new detected 'agents' for each actual node.
+        Dictionary key corresponds to node that detected the agent.
+        """
+        # TODO: If this is moved inside the loop, nothing should break?
+        detected_agents = {}
+        while True:
+            for agent in self.data_pipe:
+                results = self.process_agent_data(self.data_pipe[agent])
+                detected_agents[agent] = results
+
+            # Processed_agents contains all agents and their detections processed.
+            # This is essentially the "3D" world. This object is what will be
+            # used for final analysis and processing.
+            processed_agents = self.process(detected_agents)
+
+            # Most of the actual analysis happens here to understand the 3D world
+            world: World = self.analyze(processed_agents)
+
+            # Store the results, which will be saved under results/results.json
+            self.result_storage_pipe['processing_results'].append(world)
+
+            yield self.env.timeout(1)
         
