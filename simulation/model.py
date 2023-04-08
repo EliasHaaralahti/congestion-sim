149e29638cf2cd221b18fa03148ad0aff81c787a
unknown <haaralahti.elias@gmail.com>
2023-04-08 23:36:22 +0300

    Accept model to be used as parameter matching main.py argument options

--- simulation/model.py
+++ simulation/model.py
@@ -9,11 +9,19 @@ import torch
 # TODO: To fix the current issue where the input image is of wrong type
 # https://github.com/ultralytics/yolov5/issues/36 detailed version could work.
 
+models = {
+    "nano": "yolov5x",
+    "small": "yolov5s",
+    "medium": "yolov5m",
+    "large": "yolov5l",
+    "xlarge": "yolov5x"
+}
 
 class Model:
-    def __init__(self):
+    def __init__(self, model_name):
+        model = models[model_name]
         self.model = torch.hub.load(
-            'ultralytics/yolov5', 'yolov5x', pretrained=True)
+            'ultralytics/yolov5', model, pretrained=True)
 
     def forward(self, image: np.ndarray) -> object:
         return self.model(image)
